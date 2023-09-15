afb4066cb4a80408ca8cc5052534f2d45ec7265f
ASCIIbar <140730070+escobarpython@users.noreply.github.com>
2023-09-15 13:12:42 -0300

    Master Update JS - 1
    - Correção de Bugs & Afins
    - Nova variavel: Nota Indicada (1.5 a mais)
    - Código Reescrito

--- script.js
+++ script.js
@@ -10,19 +10,21 @@ document.addEventListener("DOMContentLoaded", function () {
         const p2 = parseFloat(p2Input.value);
         const iaf = parseFloat(iafInput.value);
 
-        // Verificar se todos os campos foram preenchidos
+        // Verificar se todos os campos foram preenchidos corretamente
         if (isNaN(p1) || isNaN(p2) || isNaN(iaf)) {
-            resultadoDiv.textContent = "Por favor, preencha todas as notas.";
+            resultadoDiv.textContent = "Por favor, preencha todas as notas corretamente.";
         } else if (p1 < 0 || p1 > 3 || p2 < 0 || p2 > 2 || iaf < 0 || iaf > 2) {
             resultadoDiv.textContent = "Por favor, insira notas dentro dos limites permitidos.";
         } else {
-            const provas1 = p1 + p2 + iaf;
-            const notaNecessariaNaPR = (7 - provas1) * 2;
+            const notasNormais = p1 + p2 + iaf;
+            const notaDesejada = notasNormais + 1.5;
+
+            const notaNecessariaNaPR = (notaDesejada * 2) - notasNormais;
 
             if (notaNecessariaNaPR < 0) {
-                resultadoDiv.textContent = "Acima da Média.";
+                resultadoDiv.textContent = "Você já atingiu a média.";
             } else {
-                resultadoDiv.textContent = `Nota necessária na PR: ${notaNecessariaNaPR.toFixed(2)}`;
+                resultadoDiv.textContent = `Nota indicada: ${notaNecessariaNaPR.toFixed(2)}`;
             }
         }
     });
